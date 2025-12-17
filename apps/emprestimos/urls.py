ab3bd14e0924cceb8124be2ba41bb3be3836724e
Jefferson J. P. Coimbra <jefferson.coimbra97@gmail.com>
2025-12-16 21:58:57 -0300

    Update urls.py

--- apps/emprestimos/urls.py
+++ apps/emprestimos/urls.py
@@ -7,6 +7,6 @@ app_name = 'apps.emprestimos'
 urlpatterns = [
     path('inserir_emprestimo/', views.inserir_emprestimo, name='inserir_emprestimo'),
     path('listar_emprestimos/', views.listar_emprestimos, name='listar_emprestimos'),
-    #path('editar_emprestimo/<int:id>/', views.editar_emprestimo, name='editar_emprestimo'),
-    #path('excluir_emprestimo/<int:id>/', views.excluir_emprestimo, name='excluir_emprestimo'),
+    path('editar_emprestimo/<int:id>/', views.editar_emprestimo, name='editar_emprestimo'),
+    path('excluir_emprestimo/<int:id>/', views.excluir_emprestimo, name='excluir_emprestimo'),
 ]
