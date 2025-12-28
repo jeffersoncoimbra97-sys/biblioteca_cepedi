aa6c18227c136d63183e06db4c055d7abef48f92
Jefferson J. P. Coimbra <jefferson.coimbra97@gmail.com>
2025-12-28 15:02:30 -0300

    Update urls.py
    Adicionar foto

--- biblioteca/urls.py
+++ biblioteca/urls.py
@@ -2,11 +2,18 @@ from django.contrib import admin
 from django.urls import path, include
 from apps.core.views import index
 
+
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
-    # Inclui todas as URLs do app alunos sob o prefixo '/alunos/'
     path('alunos/', include('apps.alunos.urls', namespace='alunos')),
     path('livros/', include('apps.livros.urls', namespace='livros')),
     path('emprestimos/', include('apps.emprestimos.urls', namespace='emprestimos')),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
